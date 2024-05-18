85a95f322618204f489f229c5634b5a4e475ec22
ValduZiedovas <pauliustrainavicius@gmail.com>
2024-05-18 11:55:14 +0300

    Merge effects + physics

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -4,10 +4,19 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform target;
-    public float smoothSpeed = 0.125f;
-    public Vector3 offset;
+    public GameObject target;
+    public float xOffset;
+    public float yOffset;
+    public float smoothSpeed = 0.125f; // The speed at which the camera follows the player
+    public Vector3 offset; // The offset from the player
 
+    void Update()
+    {
+        transform.position = new Vector3(target.transform.position.x, target.transform.position.y, -10) + new Vector3(xOffset, yOffset, -10);  
+    }
+
+
+    /*
     void FixedUpdate()
     {
         if (target != null)
@@ -16,5 +25,5 @@ public class CameraFollow : MonoBehaviour
             Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
             transform.position = smoothedPosition;
         }
-    }
+    }*/
 }
